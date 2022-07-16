b2b4e9db84511d41c41c884813a8bf0f749b26d7
Pedro Lérida Nieto <pedroleridanieto@hotmail.com>
2022-07-16 22:12:49 +0200

    Iteration#7

--- app.js
+++ app.js
@@ -164,6 +164,43 @@ console.log(newPlacesToTravel2);
  console.log(newToys);
 
 
+ /*------------------------------------------------------------------------
+ --------------**Iteración #7: For...of avanzado--------------------------
+ ------------------------------------------------------------------------*/
+
+ /*Usa un bucle **for...of** para recorrer todos los juguetes y añade los que 
+ tengan más de 15 ventas (sellCount) al array popularToys. Imprimelo por consola.. 
+ Puedes usar este array:*/
+
+
+const popularToys = [];
+const toys2 = [
+  { id: 5, name: "Buzz MyYear", sellCount: 10 },
+  { id: 11, name: "Action Woman", sellCount: 24 },
+  { id: 23, name: "Barbie Man", sellCount: 15 },
+  { id: 40, name: "El gato con Guantes", sellCount: 8 },
+  { id: 40, name: "El gato felix", sellCount: 35 },
+];
+
+for (juguetes of toys2){
+    for (key in juguetes){
+        if(key=="sellCount"){
+            if (juguetes[key]>15){
+                popularToys.push(juguetes); 
+            }
+        }
+    }
+}
+console.log(popularToys);
+
+
+
+
+
+
+
+
+
  
 
  
