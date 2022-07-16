f8418e4f4146e267ef80e51b3af1989548da067f
Pedro Lérida Nieto <pedroleridanieto@hotmail.com>
2022-07-16 19:19:48 +0200

    Iteration#3

--- app.js
+++ app.js
@@ -57,6 +57,36 @@ products.forEach((element) =>{
 
 console.log(alumns);
 
+/*------------------------------------------------------------------------
+ ----------------**Iteración #3: Probando For...of------------------------
+ -----------------------------------------------------------------------*/
+
+ /*Usa un bucle for of para recorrer todos los destinos del array. 
+ Imprime en un **_console.log_** sus valores.
+
+ Puedes usar este array:*/
+ 
+ 
+ const placesToTravel = [
+   "Japon",
+   "Venecia",
+   "Murcia",
+   "Santander",
+   "Filipinas",
+   "Madagascar",
+ ];
+
+ for (let lugares of placesToTravel){
+    console.log(lugares);
+ }
+
+
+
+
+
+ 
+
+
 
 
 
