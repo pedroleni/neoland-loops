5aafefccaae24abd08bf3b9b2d95ae652936005b
Pedro Lérida Nieto <pedroleridanieto@hotmail.com>
2022-07-16 19:13:50 +0200

    Iteration#2

--- app.js
+++ app.js
@@ -20,3 +20,48 @@ const products = [
 products.forEach((element) =>{
     if(element.includes("Camiseta")) console.log(element); 
 });
+
+/*------------------------------------------------------------------------
+ ----------------**Iteración #2: Condicionales avanzados------------------
+ -----------------------------------------------------------------------*/
+ /*Comprueba en cada uno de los usuarios que tenga al menos dos trimestres aprobados
+  y añade la propiedad isApproved a true o false en consecuencia. 
+  Una vez lo tengas compruébalo con un **_console.log_**. 
+ Puedes usar este array para probar tu funcion:*/
+ 
+ 
+ const alumns = [
+   { name: "Pepe Viruela", T1: false, T2: false, T3: true },
+   { name: "Lucia Aranda", T1: true, T2: false, T3: true },
+   { name: "Juan Miranda", T1: false, T2: true, T3: true },
+   { name: "Alfredo Blanco", T1: false, T2: false, T3: false },
+   { name: "Raquel Benito", T1: true, T2: true, T3: true },
+ ]
+
+//element[key] para sacar el value.
+
+ alumns.forEach((element)=>{
+    let contador=0;
+    
+    for ( key in element ){
+        if(element[key]==true){
+            contador++;
+        }
+    }
+    if(contador>=2){
+        element.isApproved =true;
+        
+    } else{ element.isApproved =false; }
+    
+})
+
+console.log(alumns);
+
+
+
+
+
+
+ 
+
+
