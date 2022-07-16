b9f486978a424f3c0e6c345ae2bca354951f7a28
Pedro Lérida Nieto <pedroleridanieto@hotmail.com>
2022-07-16 19:25:41 +0200

    Iteration#4

--- app.js
+++ app.js
@@ -81,6 +81,28 @@ console.log(alumns);
  }
 
 
+ /*------------------------------------------------------------------------
+ ----------------**Iteración #4: Probando For...in------------------------
+ -----------------------------------------------------------------------*/
+
+
+ /*Usa un **for...in** para imprimir por consola los datos del alienígena.. Puedes usar este objeto:*/
+
+
+const alien = {
+  name: "Wormuck",
+  race: "Cucusumusu",
+  planet: "Eden",
+  weight: "259kg",
+};
+
+
+for (key in alien){
+    console.log( key+": "+alien[key]);
+}
+
+
+
 
 
 
